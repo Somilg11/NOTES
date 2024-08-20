f13574ea1b82b03b96b17e823c20b5df38203fc2
Somilg11 <gsomil93@gmail.com>
2024-08-20 12:34:21 +0530

    getting started with rust

---
+++ Rust/hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\Somil Gupta\OneDrive\Desktop\WEB\NOTES\Rust\hello_cargo\target\debug\deps\hello_cargo.exe: src/main.rs
+
+C:\Users\Somil Gupta\OneDrive\Desktop\WEB\NOTES\Rust\hello_cargo\target\debug\deps\hello_cargo.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\Somil\ Gupta\OneDrive\Desktop\WEB\NOTES\Rust\hello_cargo\target\debug\hello_cargo.exe: C:\Users\Somil\ Gupta\OneDrive\Desktop\WEB\NOTES\Rust\hello_cargo\src\main.rs
